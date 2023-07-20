5aa8f07bfbeb8a1d9ae60e087be8082845c221eb
Kevin Nichols <kevnichols2@gmail.com>
2023-07-19 23:00:28 -0400

    turned off debug

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///initiative-role'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['ENV'] = 'development'
-app.config['DEBUG'] = True
-app.config['TESTING'] = True
+# app.config['ENV'] = 'development'
+# app.config['DEBUG'] = True
+# app.config['TESTING'] = True
 
 connect_db(app)
 db.create_all()
@@ -25,9 +25,9 @@ app.config['SECRET_KEY'] = "supersecret"
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
 #
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
 
 
 ##############################################################################
